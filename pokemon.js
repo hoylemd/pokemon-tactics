ba5acbaddde844a932d07a2090ce86630d6ba037
Michael D. Hoyle <hoylemd@gmail.com>
2013-03-23 10:50:42 -0400

    added pokemon and technique files

---
+++ pokemon.js
@@ -0,0 +1,237 @@
+// Pokemon object (unmanaged)
+var POKE_pokemon = function (context, content, spriteManager, particle_manager) {
+	// check the params
+	if (!context) {
+		throw "no context given to Pokemon class constructor";
+	} else if (!content) {
+		throw "no content given to Pokemon class constructor";
+	} else if (!spriteManager) {
+		throw "no sprite manager given to Pokemon class constructor";
+	} else if (!particle_manager) {
+		throw "no particle manager given to Pokemon class constructor";
+	}
+
+	// tile constant
+	var tileSize = 100;
+
+	// function to snap coordinates to the grid
+	var snapToGrid = function(position) {
+		// calculate offset
+		var xOff = position.x % tileSize;
+		var yOff = position.y % tileSize;
+
+		// return new coords
+		return {
+			x: position.x - xOff + (tileSize / 2),
+			y: position.y - yOff + (tileSize / 2)
+		};
+	}
+
+	// function for when the pokemon is destroyed
+	var faint =  function (that) {
+		// turn the pokemon sprite into debris
+		new particle_manager.Debris(that.sprite);
+
+		console.log("Unit " + that.name + " destroyed!");
+
+		// destroy stuff
+		that.health_bar.dispose();
+		that.destroyed = true;
+
+		// clear orders
+		that.orders.attack = null;
+		that.carryOut();
+	};
+
+	// prototype
+	var pokemon_prototype = {
+		// arbitrary movement method
+		moveTo : function (position) {
+			var bar_y = 0;
+			// snap to the grid
+			this.position = snapToGrid(position);
+
+			// move the sprites
+			this.sprite.centerOn(this.position);
+			bar_y = (this.position.y + 0.5 * this.sprite.frameDims.y) - 15;
+			this.health_bar.centerOn({x: this.position.x, y: bar_y });
+		},
+
+		// arbtrary rotation function
+		rotateTo: function(rads) {
+			this.sprite.rotateTo(rads);
+		},
+
+		// order line drawing method
+		drawLines: function() {
+			// calculate tile offset
+			var tileOffset = tileSize / 2;
+			var index = 0;
+			var order = null;
+
+			// draw the order lines if they exist
+			for (index in this.orders) {
+				order = this.orders[index];
+				context.reset();
+
+				// set up line drawing
+				context.beginPath();
+				context.lineWidth = 1;
+				context.strokeStyle = order.colour;
+
+				// draw the line
+				if (order.pending ) {
+					context.dashedLineTo(this.position, order.position,
+						[20, 20]);
+				} else {
+					context.moveTo(this.position.x ,this.position.y);
+					context.lineTo(order.position.x, order.position.y);
+				}
+				context.stroke();
+			}
+		},
+
+		// technique registration
+		addTechnique : function (technique) {
+			if (technique) {
+				this.technique = technique;
+				technique.install(this);
+			} else {
+				console.log("Cannot add a null technique");
+			}
+		},
+
+		// Hull manipulation
+		setHull : function (hull) {
+			this.hullMax = hull;
+			this.hullCurrent = hull;
+		},
+
+		// order registration
+		registerOrder : function (order) {
+			if (order.name === "attack") {
+				order.x = order.target.x;
+				order.y = order.target.y;
+				order.owner = this;
+				this.orders.attack = order;
+			}
+		},
+
+		// carry out orders function
+		carryOut : function () {
+			var attack = this.orders.attack;
+
+			// attack order
+			if (attack) {
+				if (attack.pending) {
+					if (this.technique) {
+						this.technique.setTarget(attack.target);
+						attack.pending = false;
+					} else {
+						console.log(this.name +
+							" has no technique! It cannot attack!");
+						this.orders.attack = null;
+					}
+				}
+			} else {
+				this.technique.setTarget(null);
+			}
+		},
+
+		// damage pokemon
+		takeDamage : function (amount, position) {
+			this.hullCurrent -= amount;
+
+			// check if the pokemon is fainted
+			if (this.hullCurrent <= 0) {
+				faint(this);
+			}
+		},
+
+		// function to get a pokemons dodge roll
+		dodgeRoll : function () {
+			return Math.dx(this.dodgeBonus);
+		},
+
+		// point collision function
+		collide : function(point) {
+			var dx = point.x - (this.position.x - (tileSize / 2));
+			var dy = point.y - (this.position.y - (tileSize / 2));
+
+			var collision = ((dx > 0 && dx < tileSize) &&
+				(dy > 0 && dy < tileSize)) && !this.destroyed;
+
+			return collision;
+
+		},
+
+		// order cancelling function
+		clearOrder : function(key) {
+			delete this.orders[key];
+		},
+
+		// update function
+		update : function (elapsed_ms) {
+			// update children
+			this.technique.update(elapsed_ms);
+			this.health_bar.value = this.hullCurrent / this.hullMax;
+
+			// check if attack order is still valid
+			if (this.orders.attack) {
+				if (this.orders.attack.target.destroyed) {
+					delete this.orders.attack;
+				}
+			}
+
+			// update positioning
+			this.position = this.sprite.center();
+			this.rotation = this.sprite.rotation;
+		},
+
+		// function to register the fleetView
+		registerView : function (fleetView) {
+			this.fleetView = fleetView;
+		},
+
+		// destructor
+		dispose : function () {
+			// clean up sprites
+			this.health_bar.dispose();
+			delete this.health_bar;
+			this.sprite.dispose();
+			delete this.sprite;
+		}
+	}
+
+	// constructor
+	return function(texture, mapDims, msBetweenFrames) {
+		// prepare sprites
+		var sprite = new spriteManager.Sprite(
+			(content[texture]), mapDims, msBetweenFrames);
+		var health_bar_dims = {x: sprite.frameDims.x * 0.8, y: 10};
+		var health_bar = new spriteManager.BarSprite(health_bar_dims,
+			"green", "red", "yellow", 0.2);
+
+		// build the object
+		this.__proto__ = pokemon_prototype;
+
+		// sprites
+		this.sprite= sprite;
+		this.health_bar = health_bar;
+
+		// general stuff
+		this.name= "Unnamed Unit";
+		this.position = sprite.position;
+		this.rotation = sprite.rotation;
+		this.fleetView = null;
+
+		// combat stats
+		this.orders = {};
+		this.hullMax = 5;
+		this.hullCurrent = 5;
+		this.dodgeBonus = 10;
+		this.technique = null;
+		this.destroyed = false
+
+	}
+}
