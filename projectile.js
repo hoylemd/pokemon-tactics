2bb75a3f0499a0e292386b86e6b97a8dc87089d4
Michael D. Hoyle <hoylemd@gmail.com>
2013-03-23 15:45:33 -0400

    changed to hold more information about an attack

--- projectile.js
+++ projectile.js
@@ -4,7 +4,7 @@ function ISIS_ProjectileManager () {
 
 	// function to spawn the hit text sprite
 	var spawnHitText = function (that) {
-		var hitText = "" + (that.hit ? that.technique.damage : "Miss");
+		var hitText = "" + that.technique.damage;
 		var sprite = new that.manager.sprite_manager.TextSprite(
 			hitText, "14pt Courier", "#888888");
 		sprite.centerOn(that.position);
@@ -13,41 +13,38 @@ function ISIS_ProjectileManager () {
 			sprite, destination, 1500, 0, true);
 	};
 
-	// update function for missed projectules
-	var updateMissed = function (that) {
-		that.sprite.move(that.displacement);
-		that.position = that.sprite.position;
-
-		if (that.manager.sprite_manager.canvas.boundSprite(that)) {
-			return updateMissed;
-		} else {
-			that.dispose();
-			return null;
-		}
-	};
-
 	// transition function for when the projectile collides with it's target
-	var transitionCollision = function (that) {
+	var processCollision = function (that, collider) {
 		spawnHitText(that);
-		if (that.hit) {
-			console.log(that.technique.name  +" hits " + that.target.name +
-				" for " + that.technique.damage + " points of damage");
-			that.target.takeDamage(that.technique.damage, that.position);
-			that.dispose();
-			return null;
-		} else {
-			console.log(that.technique.name + " misses " + that.target.name);
-			return updateMissed;
-		}
+		collider.takeDamage(that.damage);
 	};
 
 	// update function for incoming projectiles
 	var updateIncoming = function (that) {
+		var i = 0;
 		that.sprite.move(that.displacement);
 		that.position = that.sprite.position;
 
-		if (that.target.collide(that.position)) {
-			return transitionCollision(that);
+		var colliders = that.manager.pokemon_manager.checkForCollisions(
+			that.position, [that.owner]);
+
+		// remove the owner from the collider list
+		for (i in colliders) {
+			var current = colliders[i];
+			if (that.owner === current) {
+				colliders.splice(i, 1);
+			};
+		}
+
+		if (colliders.length > 0) {
+			for (var i in colliders) {
+				processCollision(that, colliders[i]);
+			}
+			that.dispose();
+			return null;
+		} else if (!that.manager.sprite_manager.canvas.boundSprite(that)) {
+			that.dispose();
+			return null;
 		} else {
 			return updateIncoming;
 		}
@@ -74,8 +71,8 @@ function ISIS_ProjectileManager () {
 
 	// Constructor constructor
 	var projectileConstructor = function (manager) {
-		return function (sprite, origin, target, hit, technique) {
-			// chack the args
+		return function (sprite, origin, target, technique) {
+			// check the args
 			if (sprite && origin && target && technique) {
 				// prototype it
 				this.__proto__ = projectile_prototype;
@@ -84,21 +81,20 @@ function ISIS_ProjectileManager () {
 				// set the links
 				this.sprite = sprite;
 				this.target = target;
-				this.hit = hit ? true : false;
 				this.technique = technique;
 				this.owner = technique.owner;
+				this.damage = technique.damage;
 
 				manager.add(this);
 
 				// position the sprite
 				sprite.centerOn(origin);
 				this.position = sprite.position;
-				this.distance = 3000;
-				sprite.rotation = technique.owner.rotation;
 
 				// calculate vectors
-				var vector = Math.calcVector(technique.owner.position, target.position);
+				var vector = Math.calcVector(technique.owner.position, target);
 				this.displacement = Math.multVector(vector, technique.proj_speed);
+				sprite.rotation = Math.calcVectorAngle(vector);
 
 			} else {
 				// error on bad args
@@ -108,11 +104,12 @@ function ISIS_ProjectileManager () {
 	};
 
 	// manager constructor
-	return function (sprite_manager, particle_manager) {
+	return function (sprite_manager, particle_manager, pokemon_manager) {
 		this.__proto__ = new ISIS.Manager();
 		this.Projectile = projectileConstructor(this);
 		this.sprite_manager = sprite_manager;
 		this.particle_manager = particle_manager;
+		this.pokemon_manager = pokemon_manager;
 		this.object_list = [];
 	}
 }
